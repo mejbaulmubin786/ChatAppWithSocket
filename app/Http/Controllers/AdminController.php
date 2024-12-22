015f58f65ff3317a41ea70f3b0e426951e272fbd
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-12-23 01:37:41 +0600

    Admin and Vendoer Controller created

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AdminController extends Controller {
+    public function AdminDashboard() {
+        return view('admin.admin_dashboard');
+    } // End Method
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,4 +29,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
