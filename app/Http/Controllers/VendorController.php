1b20f8852d3a235713806a260d032331681ddf24
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-07-30 19:48:34 +0600

    initial commit for fresh project

--- app/Http/Controllers/VendorController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers;
-
-class VendorController extends Controller {
-    public function VendorDashboard() {
-        return view('vendor.vendor_dashboard');
-    } // End Method
-}
