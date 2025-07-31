9a6283396f511d38a3018a2f91535779e7a85d6c
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-07-31 23:32:59 +0600

    add handleTodo function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  let [input, setInput]=useState("")
   let handelChange =(e)=>{
-    console.log(e.target.value);
+    setInput(e.target.value);
+  }
+
+  let handleTodo =()=>{
+    console.log(input);
   }
   return (
     <>
       <input onChange={handelChange} type="text" />
-      <button>Add ToDo</button>
+      <button onClick={handleTodo}>Add ToDo</button>
     </>
   )
 }
