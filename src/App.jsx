08b3de02754749db7e9932a585ff192428268feb
mejbaulmubin786 <mejbaul_mubin@yahoo.com>
2025-08-02 11:20:26 +0600

    fixed problem

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,25 @@ import { useState } from 'react'
 const App = () => {
   let [input, setInput]=useState("")
   let [arr, setArr]=useState([])
+  /*
   let handelChange =(e)=>{
     setInput(e.target.value);
   }
+  */
 
   let handleTodo =()=>{
-    let arr2 = [...arr]
-    arr2.push(input)
-    setArr(arr2)
+    //let arr2 = [...arr]
+    //arr2.push(input)
+    //setArr(arr2)
+    setArr([...arr, input]);
+    setInput("");
+    
   }
 
   
   return (
     <>
-      <input onChange={handelChange} type="text" />
+      <input onChange={(e)=>setInput(e.target.value) /*handelChange*/} type="text" />
       <button onClick={handleTodo}>Add ToDo</button>
       <ul>
         {
