06a5f2cf367ae67e5395c34e41eab58a482e7506
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-08-22 12:42:09 +0600

    loder added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
 import React from 'react'
 import { useState } from 'react'
 import { getDatabase, ref, set, push } from "firebase/database";
+import { RotatingLines } from 'react-loader-spinner';
 
 const App = () => {
   const db = getDatabase();
-  let [input, setInput]=useState("")
-  let [arr, setArr]=useState([])
+  let [input, setInput] = useState("")
+  let [arr, setArr] = useState([])
 
-  let handleTodo =()=>{
+  let handleTodo = () => {
     set(push(ref(db, 'alldata/')), {
-    name: input,
-    
-  }).then(()=>{
-    console.log("send date to database")
-    setInput("")
-  });
+      name: input,
+
+    }).then(() => {
+      console.log("send date to database")
+      setInput("")
+    });
   }
 
-  
+
   return (
     <>
-      <input onChange={(e)=>setInput(e.target.value) /*handelChange*/} type="text" />
+      <RotatingLines
+        visible={true}
+        height="96"
+        width="96"
+        color="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        ariaLabel="rotating-lines-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+
+
+
+
+      <input onChange={(e) => setInput(e.target.value) /*handelChange*/} type="text" />
       <button onClick={handleTodo}>Add ToDo</button>
       <ul>
         {
-          arr.map(item=>(
+          arr.map(item => (
             <li>{item}</li>
           ))
         }
