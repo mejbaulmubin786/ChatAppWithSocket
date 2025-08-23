3941b98ab262fc5a70298ad350bf5532738ee5f0
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-08-23 09:49:26 +0600

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,52 @@ import React from 'react'
 import { useState } from 'react'
 import { getDatabase, ref, set, push } from "firebase/database";
 import { RotatingLines } from 'react-loader-spinner';
+import Im1 from './assets/ai.gif'
 
 const App = () => {
   const db = getDatabase();
   let [input, setInput] = useState("")
-  //let [loader, setLoader] = useState(false)
+  let [loader, setLoader] = useState(false)
   //let [arr, setArr] = useState([])
 
   let handleTodo = () => {
+    setLoader(true)
     set(push(ref(db, 'alldata/')), {
       name: input,
 
     }).then(() => {
-      console.log("send date to database")
       setInput("")
+      setLoader(false)
     });
   }
 
 
   return (
     <>
-      <RotatingLines
-        visible={true}
-        height="96"
-        width="96"
-        color="grey"
-        strokeWidth="5"
-        animationDuration="0.75"
-        ariaLabel="rotating-lines-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-      />
 
 
 
 
       <input onChange={(e) => setInput(e.target.value) /*handelChange*/} type="text" />
-      <button onClick={handleTodo}>Add ToDo</button>
+
+      {
+        loader ?
+          <RotatingLines
+            visible={true}
+            height="40"
+            width="40"
+            color="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            ariaLabel="rotating-lines-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+          //<img src={Im1} />
+          //<h1>Loading Data..............</h1>
+          :
+          <button onClick={handleTodo}>Add ToDo</button>
+      }
       {/*<ul>
         {
           arr.map(item => (
