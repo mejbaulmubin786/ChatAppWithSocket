58d3b2cd4bdf783c2339b4959caa30ab4b7ac0f8
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-11-26 15:53:18 +0600

    update home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
 
 const HomePage = () => {
+    const [selectedUser, setSelectedUser] = useState(false);
     return (
         <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-            <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-higgen h-[100%] grid grid-cols-1 relative'>
+            <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-higgen h-[100%] grid grid-cols-1 relative ${selectedUser ?
+                'md:grid-colos-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr' : 'md:grid-cols-2'}`
+                }>
                 <Sidebar/>
                 <ChatContainer/>
                 <RightSidebar/>
