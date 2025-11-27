20c130f0e36cb392da0a7b6534283638d6cd31f6
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-11-27 16:01:12 +0600

    update

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import assets from '../assets/assets'
 
-const Sidebar = () => {
+const Sidebar = ({selectedUser, setSelectedUser}) => {
   return (
-    <div>Sidebar</div>
+    <div>
+      <div className='pb-5'>
+        <div className='flex justify-between items-center'>
+          <img src={assets.logo} alt="logo" className='max-w-40'/>
+          <div className='relativge py-2 group'>
+            <img src={assets.menu_icon} alt="logo" className='max-w-5 cursor-pointer'/>
+            <div>
+              <p>Edit Profile</p>
+              <hr className='my-2 border-t border-gray-500'/>
+              <p>Logout</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
