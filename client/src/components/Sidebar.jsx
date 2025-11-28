a6381314f85b9a7d175c9739aa179d30632cdf07
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-11-28 22:21:05 +0600

    update sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import assets from '../assets/assets'
 
-const Sidebar = ({selectedUser, setSelectedUser}) => {
+const Sidebar = ({ selectedUser, setSelectedUser }) => {
   return (
     <div>
       <div className='pb-5'>
         <div className='flex justify-between items-center'>
-          <img src={assets.logo} alt="logo" className='max-w-40'/>
+          <img src={assets.logo} alt="logo" className='max-w-40' />
           <div className='relativge py-2 group'>
-            <img src={assets.menu_icon} alt="logo" className='max-w-5 cursor-pointer'/>
+            <img src={assets.menu_icon} alt="logo" className='max-w-5 cursor-pointer' />
             <div>
-              <p>Edit Profile</p>
-              <hr className='my-2 border-t border-gray-500'/>
+              <p className='cursor-pointer text-sm'>Edit Profile</p>
+              <hr className='my-2 border-t border-gray-500' />
               <p>Logout</p>
             </div>
           </div>
